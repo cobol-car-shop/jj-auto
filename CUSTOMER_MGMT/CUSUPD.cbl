0a68188f42d7dc45034268b9b8673516d2577391
walkerj015 <walkerj015@mymail.mssu.edu>
2019-03-27 20:11:20 -0500

    Updates

--- CUSTOMER_MGMT/CUSUPD.cbl
+++ CUSTOMER_MGMT/CUSUPD.cbl
@@ -5,16 +5,16 @@
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-           SELECT OPTIONAL CUSTOMER-INFO
+           SELECT CUS-FILE
            ASSIGN TO 'CUSTOMER.IDX'
                ORGANIZATION IS INDEXED
                ACCESS IS SEQUENTIAL
                RECORD KEY IS CUST-ID-REC
-               ALTERNATE RECORD KEY IS CUST-LNAME-REC.
+               ALTERNATE RECORD KEY IS CUST-LNAME-REC WITH DUPLICATES.
 
        DATA DIVISION.
        FILE SECTION.
-       FD  CUSTOMER-INFO.
+       FD  CUS-FILE.
        01  CUST-REC.
            05  CUST-ID-REC                      PIC 9(5).
            05  CUST-FNAME-REC                   PIC X(15).
@@ -28,8 +28,13 @@
            05  CUST-DST-REC                     PIC X.
 
        WORKING-STORAGE SECTION.
+       01  CUS-SRCH-INFO.
+           05  CUS-ID-IN                       PIC 9(5).
+           05  CUS-FNAME-IN                    PIC X(15).
+           05  CUS-LNAME-IN                    PIC X(15).
+
        01  TRANS-REC-IN.
-           05  TRANS-ID-IN                      PIC 9(5)  VALUE 00001.
+           05  TRANS-ID-IN                      PIC 9(5).
            05  TRANS-FNAME-IN                   PIC X(15).
            05  TRANS-LNAME-IN                   PIC X(15).
            05  TRANS-PHONE-IN                   PIC 9(10).
@@ -44,7 +49,7 @@
        01  WORK-AREAS.
            05  MORE-RECS          PIC X(1)
                   VALUE 'Y'.
-           05  MSSG-OUT           PIC X(46).
+           05  MSSG-OUT           PIC X(50).
            05  DATA-OK                         PIC X(1).
            05  WS-DATE.
                10  WS-YEAR                     PIC 9999.
@@ -87,10 +92,12 @@
                10 LINE 13 COLUMN 20       VALUE "Or".
                10 LINE 14 COLUMN 20       VALUE "Customer First Name: ".
                10 LINE 15 COLUMN 20       VALUE "Customer Last  Name: ".
-           05  INPUT-FIELDS.
-               10 LINE 12 COLUMN 33       PIC 9(5) TO TRANS-ID-IN.
-               10 LINE 14 COLUMN 41       PIC X(15) TO TRANS-FNAME-IN.
-               10 LINE 15 COLUMN 41       PIC X(15) TO TRANS-LNAME-IN.
+           05  INPUT-FIELDS
+                   REVERSE-VIDEO
+                   AUTO.
+               10 LINE 12 COLUMN 33       PIC 9(5)  TO CUS-ID-IN.
+               10 LINE 14 COLUMN 41       PIC X(15) TO CUS-FNAME-IN.
+               10 LINE 15 COLUMN 41       PIC X(15) TO CUS-LNAME-IN.
 
        01  SCREEN-1.
            05  BLANK SCREEN
@@ -99,7 +106,6 @@
            05  INPUT-PROMPTS.
                10  LINE 8 COLUMN 20       VALUE "FIRST NAME: ".
                10  LINE PLUS 2 COLUMN 20  VALUE "LAST NAME: ".
-               10  LINE PLUS 2 COLUMN 20  VALUE "MIDDLE INITIAL: ".
                10  LINE PLUS 2 COLUMN 20  VALUE "PHONE NUMBER: ".
                10  LINE PLUS 2 COLUMN 20  VALUE "EMAIL ADDRESS: ".
                10  LINE PLUS 2 COLUMN 20  VALUE "STREET ADDRESS: ".
@@ -109,19 +115,27 @@
            05  INPUT-FIELDS
                    REVERSE-VIDEO
                    AUTO.
-               10  LINE 8 COLUMN 39        PIC X(20) TO TRANS-FNAME-IN.
-               10  LINE PLUS 2 COLUMN 39   PIC X(20) TO TRANS-LNAME-IN.
-               10  LINE PLUS 2 COLUMN 39   PIC 9(10) TO TRANS-PHONE-IN.
-               10  LINE PLUS 2 COLUMN 39   PIC X(35) TO TRANS-EMAIL-IN.
-               10  LINE PLUS 2 COLUMN 39   PIC X(35) TO TRANS-ADDRS-IN.
-               10  LINE PLUS 2 COLUMN 39   PIC X(15) TO TRANS-CITY-IN.
-               10  LINE PLUS 2 COLUMN 39   PIC XX TO TRANS-STATE-IN.
-               10  LINE PLUS 2 COLUMN 39   PIC 9(5) TO TRANS-ZIP-IN.
+               10  LINE 8 COLUMN 39        PIC X(20)
+                                USING TRANS-FNAME-IN.
+               10  LINE PLUS 2 COLUMN 39   PIC X(20)
+                                USING TRANS-LNAME-IN.
+               10  LINE PLUS 2 COLUMN 39   PIC 9(10)
+                                USING TRANS-PHONE-IN.
+               10  LINE PLUS 2 COLUMN 39   PIC X(35)
+                                USING TRANS-EMAIL-IN.
+               10  LINE PLUS 2 COLUMN 39   PIC X(35)
+                                USING TRANS-ADDRS-IN.
+               10  LINE PLUS 2 COLUMN 39   PIC X(15)
+                                 USING TRANS-CITY-IN.
+               10  LINE PLUS 2 COLUMN 39   PIC XX
+                                USING TRANS-STATE-IN.
+               10  LINE PLUS 2 COLUMN 39   PIC 9(5)
+                                  USING TRANS-ZIP-IN.
 
        01  SCREEN-2.
            05  BACKGROUND-COLOR BLACK
                AUTO.
-               10  LINE 18 COLUMN 20
+               10  LINE 24 COLUMN 20
                        VALUE "IS CUSTOMER INFOMATION CORRECT"
                        FOREGROUND-COLOR RED
                        HIGHLIGHT.
@@ -170,7 +184,7 @@
                MOVE " " TO MSSG-OUT
                DISPLAY UPD-SCREEN
                ACCEPT  UPD-SCREEN
-               IF TRANS-ID-IN > 0
+               IF CUS-ID-IN > 00000
                  PERFORM 300-CID-RTN
                ELSE
                  PERFORM 350-NAME-RTN
@@ -184,6 +198,10 @@
                    ACCEPT SCREEN-2
                  END-PERFORM
                  PERFORM 200-UPDATE-RTN
+               ELSE
+                 DISPLAY CLEAR-SCREEN
+                 MOVE 'Customer could not be found.'
+                      TO MSSG-OUT
                END-IF
                DISPLAY SCREEN-3
                ACCEPT SCREEN-3
@@ -191,25 +209,23 @@
            STOP RUN.
 
        200-UPDATE-RTN.
-           OPEN EXTEND CUSTOMER-INFO
-           MOVE TRANS-ID-IN TO CUST-NO-OUT
-           MOVE TRANS-FNAME-IN TO CUST-FNAME-OUT
-           MOVE TRANS-LNAME-IN TO CUST-LNAME-OUT
-           MOVE TRANS-PHONE-IN TO CUST-PHONE-OUT
-           MOVE TRANS-EMAIL-IN TO CUST-EMAIL-OUT
-           MOVE TRANS-ADDRS-IN TO CUST-ADDR-OUT
-           MOVE TRANS-CITY-IN TO CUST-CITY-OUT
-           MOVE TRANS-STATE-IN TO CUST-STATE-OUT
-           MOVE TRANS-ZIP-IN TO CUST-ZIP-OUT
-           MOVE CUST-DST-REC TO CUST-DST-OUT
-           REWRITE CUST-REC.
+           OPEN I-O CUS-FILE
+           MOVE 'N' TO DATA-OK
+           PERFORM UNTIL DATA-OK = 'F'
+             PERFORM 500-READ-RTN
+             IF CUST-ID-REC = TRANS-ID-IN
+               REWRITE CUST-REC FROM TRANS-REC-IN
+               MOVE 'F' TO DATA-OK
+             END-IF
+           END-PERFORM
+           CLOSE CUS-FILE.
 
        300-CID-RTN.
-           OPEN INPUT CUSTOMER-INFO
+           OPEN INPUT CUS-FILE
            MOVE ' ' TO DATA-OK
            PERFORM UNTIL DATA-OK = 'F' OR 'N'
              PERFORM 500-READ-RTN
-             IF CUST-ID-REC = TRANS-ID-IN
+             IF CUST-ID-REC = CUS-ID-IN
                IF CUST-DST-REC = 'N'
                  PERFORM 400-MOVE-RTN
                  MOVE 'F' TO DATA-OK
@@ -219,18 +235,17 @@
                END-IF
              END-IF
            END-PERFORM
-           MOVE ' ' TO DATA-OK
-           CLOSE CUSTOMER-INFO.
+           CLOSE CUS-FILE.
 
        350-NAME-RTN.
-           OPEN INPUT CUSTOMER-INFO
+           OPEN INPUT CUS-FILE
            MOVE ' ' TO DATA-OK
            PERFORM UNTIL DATA-OK = "F" OR "N"
              PERFORM 500-READ-RTN
              EVALUATE TRUE
-               WHEN TRANS-FNAME-IN = CUST-FNAME-REC
+               WHEN CUS-FNAME-IN = CUST-FNAME-REC
                  EVALUATE TRUE
-                   WHEN TRANS-LNAME-IN = CUST-LNAME-REC
+                   WHEN CUS-LNAME-IN = CUST-LNAME-REC
                      IF CUST-DST-REC = 'N'
                        PERFORM 400-MOVE-RTN
                        MOVE "F" TO DATA-OK
@@ -242,8 +257,7 @@
                  END-EVALUATE
              END-EVALUATE
            END-PERFORM
-           MOVE ' ' TO DATA-OK
-           CLOSE CUSTOMER-INFO.
+           CLOSE CUS-FILE.
 
        400-MOVE-RTN.
            MOVE CUST-ID-REC TO TRANS-ID-IN
@@ -257,8 +271,20 @@
            MOVE CUST-ZIP-REC TO TRANS-ZIP-IN
            MOVE CUST-DST-REC TO TRANS-DST-IN.
 
+       450-CLRFLD-RTN.
+           MOVE 0 TO TRANS-ID-IN
+           MOVE " " TO TRANS-FNAME-IN
+           MOVE " " TO TRANS-LNAME-IN
+           MOVE 0000000000 TO TRANS-PHONE-IN
+           MOVE " " TO TRANS-EMAIL-IN
+           MOVE " " TO TRANS-ADDRS-IN
+           MOVE " " TO TRANS-CITY-IN
+           MOVE " " TO TRANS-STATE-IN
+           MOVE 0 TO TRANS-ZIP-IN
+           MOVE "Y" TO TRANS-DST-IN.
+
        500-READ-RTN.
-           READ CUSTOMER-INFO
+           READ CUS-FILE
              AT END MOVE "N" TO DATA-OK
            END-READ.
 
